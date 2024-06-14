94563d37af877480a54401d13524f67eadf5c860
Combustion-is-fun <Combustionisfun@protonmail.com>
2024-06-14 13:50:30 +0100

    plop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello Edit <code>src/App.js</code> and save to reload.
+          hello bob Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,6 +19,12 @@ function App() {
         >
           Learn React
         </a>
+        <p></p>
+        <Stack spacing={2} direction="row">
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </Stack>
       </header>
     </div>
   );
